f42fba2ea303f48e9e2a3421b4e46d09e8ddbcbb
phgoff <mous17zy@hotmail.com>
2018-03-29 21:33:47 +0700

    Add Drawable Listview

--- app/src/main/java/com/phgof/app_withbutton/MainActivity.java
+++ app/src/main/java/com/phgof/app_withbutton/MainActivity.java
@@ -1,31 +1,50 @@
 package com.phgof.app_withbutton;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private View view;
-    private TextView tv2,tv3;
-    private Button btn1,btn2;
+    private TextView tv2,tv3,tvX;
+    private Button btn2;
     private RadioGroup radioGroup;
     private EditText tvname;
     ArrayList<String> items = new ArrayList<String>();
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String[] mDrawerTitle = {"New", "Custom List"};
+    private ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv2 = findViewById(R.id.tv2);
+        tvX = findViewById(R.id.tvX);
         btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
         tvname = findViewById(R.id.tvname);
         radioGroup = (RadioGroup) findViewById(R.id.radiotypes);
 
@@ -39,13 +58,37 @@ public class MainActivity extends AppCompatActivity {
                 int id = radioGroup.getCheckedRadioButtonId();
                 RadioButton radioButton = (RadioButton) findViewById(id);
                 b.putString("radiotypes", radioButton.getText().toString());
-
                 i.putExtras(b);
-
                 i.putExtra("itemslist",items);
                 startActivity(i);
             }
         });
+        // Hamburger Menu
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,
+                mDrawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer);
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mListView = (ListView) findViewById(R.id.drawer);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mDrawerTitle);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,mDrawerTitle[position],Toast.LENGTH_SHORT).show();
+                //String  itemValue    = (String) mListView.getItemAtPosition(position);
+//                mDrawerLayout.closeDrawers();
+//                tvX.setText("Position :"+ position+"  ListItem : " +itemValue);
+            }
+        });
+        //-----
     }
 
     public void onCheckboxClicked(View view) {
@@ -80,11 +123,28 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
-//    public void showitems(View view) {
-//
-//        String res = " ";
-//        for(String itemvalue : items)
-//            res += itemvalue + "\n";
-//        tv3.setText(res);
-//    }
+
+        //Hamburger
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+            //when click Hamburger will show the menu
+            if (actionBarDrawerToggle.onOptionsItemSelected(item))
+                return true;
+        return super.onOptionsItemSelected(item);
+        }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+
+
+
 }
